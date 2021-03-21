3be8687acef243ed4539c7382b24a7a7655ea021
buckleader <jasmin28virdi@gmail.com>
2021-03-22 03:12:35 +0530

    added components

---
+++ src/components/Login/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+
+
+const Login = () => {
+    return(
+    <div className="App">
+        <h1>antd version: {version}</h1>
+        <DatePicker />
+        <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+        </Button>
+    </div>
+    )
+}
+
+export default Login;
